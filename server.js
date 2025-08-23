057645711562f0a6abc086442677c9d61eae3085
NagiReddy <21cs01006@iitbbs.ac.in>
2025-08-24 00:16:07 +0530

    Remove deprecated mongoose options

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 
 app.use('/plants', plantRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('MongoDB connected');
         const PORT = process.env.PORT || 5000;
